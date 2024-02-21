a1d2ed608efe8f3a10a963e409aa5ce1f7f4dd99
AQEEL AHMED <120096273+aqeelahmed-dev@users.noreply.github.com>
2024-02-21 21:12:23 +0500

    Complete website
    Complete website Commit.!

---
+++ script.js
@@ -0,0 +1,85 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const generateButton = document.querySelector('.generate-btn');
+    const lines = document.querySelectorAll('.line');
+
+    // Initialize locked state for each line
+    const lockedLines = Array.from(lines).map(() => false);
+
+    // Set default colors and add lock icon on page load
+    setDefaultColors();
+
+    generateButton.addEventListener('click', function() {
+        lines.forEach((line, index) => {
+            // Check if the line is not locked before changing the color
+            if (!lockedLines[index]) {
+                line.style.backgroundColor = generateRandomColor();
+            }
+        });
+    });
+
+    function setDefaultColors() {
+        lines.forEach((line, index) => {
+            line.style.backgroundColor = generateRandomColor();
+            addLockIcon(line, index);
+        });
+    }
+
+    function generateRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    function addLockIcon(line, index) {
+        const lockIcon = document.createElement('i');
+        lockIcon.className = 'fa-solid fa-lock additional-icon';
+        lockIcon.onclick = function() {
+            toggleLock(index, lockIcon);
+        };
+        line.appendChild(lockIcon);
+    }
+
+    function toggleLock(index, lockIcon) {
+        lockedLines[index] = !lockedLines[index];
+        const newColor = lockedLines[index] ? 'red' : 'white';
+        lockIcon.style.color = newColor;
+    }
+});
+function copyColorCode(icon) {
+    const line = icon.parentElement;
+    const rgbColorCode = line.style.backgroundColor;
+
+    // Convert RGB to HEX
+    const hexColorCode = rgbToHex(rgbColorCode);
+
+    // Create a temporary input element to copy the color code
+    const tempInput = document.createElement('input');
+    document.body.appendChild(tempInput);
+    tempInput.value = hexColorCode;
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+
+    // Provide visual feedback to the user (optional)
+    icon.style.color = 'red';
+    setTimeout(() => {
+        icon.style.color = 'white';
+    }, 1000);
+}
+
+// Function to convert RGB to HEX
+function rgbToHex(rgb) {
+    // Extract the RGB values from the string
+    const rgbArray = rgb.match(/\d+/g);
+    
+    // Convert each value to HEX and concatenate
+    const hexCode = rgbArray.map(value => {
+        const hexValue = parseInt(value).toString(16);
+        return hexValue.length === 1 ? "0" + hexValue : hexValue;
+    }).join('');
+
+    return "#" + hexCode.toUpperCase();
+}
